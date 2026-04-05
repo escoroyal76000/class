90e31ae4972e11042530be91c9e57fb3a969fd99
escoroyal76000 <123991504+escoroyal76000@users.noreply.github.com>
2026-04-05 02:56:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,162 @@
+ function playGame(){
+       let roll1 = getRandomNumber()   
+
+       console.log("roll1:", roll1);
+
+
+       let roll2 = getRandomNumber();
+
+       console.log("roll2:", roll2);
+
+
+        let rollsum = roll1 + roll1;
+
+       console.log("rollsum:", rollsum);
+ 
+         if (roll1 == roll2 && rollsum % 2 == 0){
+               // update the message div with the good news that the round was a win
+               document.getElementById("divmessage").textContent = "You win!";
+         }
+         else if (rollsum == 7 || rollsum == 11){
+               // update the message div with the bad news that the round was a loss
+               document.getElementById("divmessage").textContent = "You lose!";
+   }
+
+       else{
+         // update the message div with the good news that the round was a tie
+         document.getElementById("divmessage").textContent = "You tied (pushed)!";
+
+         document.getElementById("divroll1").textContent = "roll1: " + roll1;
+         document.getElementById("divroll2").textContent = "roll2: " + roll2;
+         document.getElementById("divsum").textContent = "Sum: " + rollsum;   
+       }
+      }
+
+
+
+   function getRandomNumber(){
+
+
+   let number = Math.random() * 6;
+
+
+   number = Math.floor(number) + 1;
+
+   return number;
+ }
+
+  function validate(){
+
+            let firstName = document.getElementById("txtfirstname").value;
+
+            console.log("firstname: " + " " + firstName);
+
+
+             let lastName = document.getElementById("txtlastname").value;
+
+            console.log("lastname: " + lastName);
+            
+             let zip = document.getElementById("txtzip").value;
+
+            console.log("zip=" + zip);
+
+            let fullname = firstName + " " + lastName;
+
+            console.log("fullname=" + fullname);
+
+            let message = "";
+
+            if(fullname.length == 1 || fullname.length > 20){
+                message += "please enter a name that is less than 20 characters.";
+            }
+
+            else if(zip.length != 5){
+                message = "please enter a 5 digit zip code.";
+            }
+            
+
+            console.log("message=" + message);
+
+
+            document.getElementById("divmessage").textContent = message;
+
+        }
+         function checkpalin(event){
+            event.preventDefault();
+            let wordtotest= document.getElementById("txtword").value;
+           console.log("wordtotest="+ wordtotest);
+
+           let bPalin =  ispalin(wordtotest);
+
+           let divmessage = document.getElementById("divmessage");
+
+            if(bPalin){
+                divmessage.textContent = "The word/phrase is a palindrome.";
+            } 
+            
+            else {
+                divmessage.textContent = "The word/phrase is not a palindrome.";
+            }
+        }
+
+
+        function ispalin(strtotest){
+            strtotest = strtotest.toLowerCase();
+
+             console.log("strtotest="+ strtotest);
+
+             let strReverse = strtotest;
+
+                strReverse = strReverse.split("").reverse().join("");
+
+                console.log("strReverse=" + strReverse);
+
+                if(strReverse == strtotest){
+                   return true;
+                }
+                
+                return false;
+        }
+
+         function addAudio(){
+
+        let divaudio = document.getElementById("divaudio");
+
+        let audioElement = document.createElement("audio");
+        // set attibets of our new html element
+
+        audioElement.setAttribute("id", "myAudio");
+
+        audioElement.setAttribute("src", "hitslab-magic-mystery-harry-potter-music-320643.mp3");
+
+
+
+         audioElement.setAttribute("controls", "controls");
+
+         audioElement.volume = 0.5;
+
+         divaudio.appendChild(audioElement);
+
+         document.getElementById("btnAddaudio").hidden = false;
+
+         document.getElementById("").hidden = true;
+
+         document.getElementById("btnpauseaudio").hidden = true;
+
+
+
+    }
+
+    function playAudio(){
+
+        let myAudio = document.getElementById("myAudio");
+
+        myAudio.play();
+    }
+
+    function pauseAudio(){
+        let myAudio = document.getElementById("myAudio");
+
+        myAudio.pause();
+    }
+ 
